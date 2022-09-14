ef438f2fa999eab73d8a3c36c91d8403cfbcd6ef
Parkmyounghoi <57881372+dron512@users.noreply.github.com>
2022-09-14 20:06:47 +0900

    pmh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Head from './components/Head';
 import Foot from './components/Foot';
@@ -7,8 +6,10 @@ import Index from './components/Index';
 import Menu1 from './components/Menu1';
 import Menu2 from './components/Menu2';
 import Menu3 from './components/Menu3';
+
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div>
@@ -18,7 +19,7 @@ function App() {
           <Route path="/" element={<Index />}></Route>
           <Route path="/menu1" element={<Menu1 />}></Route>
           <Route path="/menu2" element={<Menu2 />}></Route>
-          <Route path="/menu3" element={<Menu3 />}></Route>
+          {/* <Route path="/menu3" element={<Menu3 />}></Route> */}
           {/* <Route path="/u/*" element={<Menu/>}></Route> */}
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
           {/* <Route path="*" element={<NotFound />}></Route> */}
--- src/components/Index.js
+++ src/components/Index.js
@@ -1,10 +1,5 @@
+import { useEffect } from 'react';
 import axios from 'axios';
-
-axios.get('https://localhost:8080/board/list').then((Response)=>{
-    console.log(Response.data);
-}).catch((Error)=>{
-    console.log(Error);
-})
 import a01 from '../img/수영장.jpg'
 import a02 from '../img/캠핑장.png'
 import b02 from '../img/캠핑장2.jpg'
@@ -13,42 +8,87 @@ import c01 from '../img/bx_car1.png'
 import c02 from '../img/bx_car2.png'
 import c03 from '../img/bx_car3.png'
 
+import cookies from 'react-cookies';
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+const expires = new Date();
+    // 년도 설정, 현재의 년도를 가져와 +10을 해서 2032가 됨
+    expires.setFullYear(expires.getFullYear() + 10);
+    cookies.save('userid', 'coadingHospital',   
+    {
+        path: '/',        // 쿠키 값을 저장하는 서버 경로
+        expires,          // 유효 시간
+        //secure: true,   // 웹 브라우저와 웹 서버가 https로 통신하는 경우에만 쿠키 저장
+        //httpOnly: true  // document.cookie라는 자바스크립트 코드로 쿠키에 비정상적으로 접속하는 것을 막는 옵션
+    }
+)
+
+const myid = getCookie('userid');
+console.log(myid)
+var test = {"content":"content"}
+
+axios.get('http://localhost:8080/board/list',test).then((Response)=>{
+    console.log(Response.data);
+}).catch((Error)=>{
+    console.log(Error);
+})
+
+const mydata = {username:"bbb",password:"password"}
+
+useEffect(()=>{
+    axios.post("http://localhost:8080/member/join", mydata)
+},[]);
+
 const Index = () => {
     return ( <div>
     <div id="root">
-        <div class="container">
-            <div class="sub_container">
-                <div class="content">
+        <div className="container">
+            <div className="subcontainer">
+                <div className="content">
                     <p><br/>나르샤에서 <br/>막힘없이 캠핑가자</p>
-                    <div class="searchbtn">
-                        <input style={{height: '43px', paddingLeft: '20px', display: 'inline !important',                boxSizing: 'border-box'}} className="www" type="text" placeholder="캠핑장 검색"/>
+                    <div className="searchbtn">
+                        <input style={{height: '43px', paddingLeft: '20px', display: 'inline !important', boxSizing: 'border-box'}} className="www" type="text" placeholder="캠핑장 검색"/>
                     </div>
-                    <a href="search" class="rrr" type="submit">캠핑장 검색</a>
-                    <div class="slider">
-                        <div><img src={c01}/></div>
-                        <div><img src={c02}/></div>
-                        <div><img src={c03} alt=""/></div>
+                    <a href="search" className="rrr" type="submit">캠핑장 검색</a>
+                    <div className="slider">
+                        <div><img src={c01} alt="c01"/></div>
+                        <div><img src={c02} alt="c02"/></div>
+                        <div><img src={c03} alt="c03"/></div>
                     </div>
                             <a href="menu2">공유하기</a>
                         <h2>캠핑 BEST PHOTO <a href="#">{'>>>'}</a></h2><br/>
                         <p>캠핑 다녀오셨나요? 추억을 남겨보세요!</p>
-                        <div class="aa">
+                        <div className="aa">
                             <div>
                                 <a href="menu.jsp">
-                                    <img src={a02} />
+                                    <img src={a02} alt="a02"/>
                                 </a>
                             </div>
                             <div>
                                 <a href="menu3.jsp">
-                                    <img src={a01}/>
+                                    <img src={a01} alt="a01"/>
                                 </a>
                             </div>
                             <div> <a href="menu4.jsp">
-                                <img src={a03} />
+                                <img src={a03} alt="a03"/>
                             </a></div>
                         <h2>캠핑 Magazine <a href="#">{'>>'}</a></h2>
-                        <div class="bb">
-                            <div><img src={b02} /></div>
+                        <div className="bb">
+                            <div><img src={b02} alt="b02" /></div>
                         </div>
                     </div>
                 </div>

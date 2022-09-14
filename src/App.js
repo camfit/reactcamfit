b939029dbe5f0adb56d8f5398d1e39f82585556d
choiminseo1 <dkdj3usjakas@naver.com>
2022-09-14 17:51:09 +0900

    76t7g
    vguvh

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Foot from './components/Foot';
 import Index from './components/Index';
 
 import Menu1 from './components/Menu1';
+import Menu2 from './components/Menu2';
+import Menu3 from './components/Menu3';
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />}></Route>
           <Route path="/menu1" element={<Menu1 />}></Route>
+          <Route path="/menu2" element={<Menu2 />}></Route>
+          <Route path="/menu3" element={<Menu3 />}></Route>
           {/* <Route path="/u/*" element={<Menu/>}></Route> */}
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
           {/* <Route path="*" element={<NotFound />}></Route> */}
---
+++ src/components/Menu2.js
@@ -0,0 +1,28 @@
+const Menu2 = () => {
+    return (
+    <div id="root">
+        <div classname="container">
+                <div classname="content">
+                    <p><br/>나르샤 프로젝트<br/>커뮤니티</p><br/><br/>
+                    <input style="height: 43px; padding-left: 20px; display: inline !important;
+                box-sizing: border-box;" classname="www" type="text" placeholder="게시물 찾기"/><br/>
+                    <a href="#" classname="rrr" type="submit">게시물 검색</a><br/><br/><br/><br/><br/>
+                    <h1 style="font-size: 24px;">커뮤니티 제목</h1><br/>
+                    <img classname="www" src="img/333.jpg" alt="구봉산오토캠핑장 나인힐스"/>
+                    <h1>커뮤니티 내용.</h1><br/>
+                    <h1 style="text-align: right">캠핑장1: OO군</h1><br/><br/><br/>
+                    <h1 style="font-size: 24px;">커뮤니티 제목2</h1><br/>
+                    <img classname="www" src="img/222.jpg" alt="구봉산오토캠핑장 나인힐스"/>
+                    <h1>커뮤니티 내용2</h1><br/>
+                    <h1 style="text-align: right">캠핑장2: OO군</h1><br/><br/><br/>
+                    <h1 style="font-size: 24px;">커뮤니티 제목3</h1><br/>
+                    <img classname="www" src="img/111.jpg"/>
+                    <h1>커뮤니티 내용3</h1><br/>
+                    <h1 style="text-align: right">캠핑장3: OO군</h1><br/><br/><br/>
+                    <a href="community" classname="rrr" type="submit">커뮤니티 글 작성하기</a>
+                </div>
+        </div>
+    </div>
+    );
+}
+export default Menu2;
